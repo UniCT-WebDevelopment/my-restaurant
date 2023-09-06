c98672d6b02395645256713f1534370106e2503b
Peppe15699 <peppepisasle@gmail.com>
2023-09-06 11:47:23 +0200

    modifiche alla home

--- public/scrip/home.js
+++ public/scrip/home.js
@@ -1,6 +1,6 @@
 
 function createProductCard(product){
-
+/*
     // Form
     const formElement = document.createElement('form');
     formElement.method = 'POST';
@@ -154,6 +154,118 @@ function createProductCard(product){
 
 
 
+    cardElement.appendChild(cardBodyElement);
+    cardElement.appendChild(formElement);
+    return cardElement;*/
+    // Card
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card', 'mt-2', 'ms-4', 'mb-4');
+    cardElement.style.width = '18rem';
+    cardElement.dataset.id = product.id;
+
+    // Div immagine
+    const imgDiv = document.createElement('div');
+    imgDiv.style.width = '100%'; // Dimensione fissa
+    imgDiv.style.height = '200px'; // Dimensione fissa (puoi cambiare questo valore a tuo piacimento)
+
+    // Immagine
+    const imgElement = document.createElement('img');
+    imgElement.src = product.percorso_img;
+    imgElement.style.width = '100%'; // Immagine si adatta alla larghezza del div
+    imgElement.style.height = '100%'; // Immagine si adatta all'altezza del div
+    imgElement.classList.add('card-img-top');
+
+
+    imgDiv.appendChild(imgElement);
+    cardElement.appendChild(imgDiv);
+
+    // BodyCard
+    const cardBodyElement = document.createElement('div');
+    cardBodyElement.classList.add('card-body', 'd-grid', 'align-items-center');
+
+    // Nome
+    const titleElement = document.createElement('h5');
+    titleElement.classList.add('card-title', 'text-center', 'mb-1');
+    titleElement.textContent = product.nome;
+    cardBodyElement.appendChild(titleElement);
+
+    // Ingredienti
+    const textElement = document.createElement('p');
+    textElement.classList.add('card-text', 'text-center', 'mb-1');
+    textElement.textContent = product.ingredienti.replace(/,/g, " - ");
+    cardBodyElement.appendChild(textElement);
+
+    // Prezzo
+    const priceElement = document.createElement('p');
+    priceElement.classList.add('card-text', 'text-center', 'mb-1');
+    priceElement.textContent = product.prezzo + ' €'
+    cardBodyElement.appendChild(priceElement);
+
+    // Form
+    const formElement = document.createElement('form');
+    formElement.method = 'POST';
+    formElement.classList.add('p-2','d-flex', 'justify-content-between');
+
+    // CSFR
+    const csrfField = document.createElement('input');
+    csrfField.type = 'hidden';
+    csrfField.name = '_token'; // Assicurati che il nome sia '_token' per essere compatibile con Laravel
+    csrfField.value = window.csrfToken; // Questo inserisce il token CSRF generato da Laravel nel campo
+    console.log(csrfField.value);
+    formElement.appendChild(csrfField);
+
+    // ID
+    const idInput = document.createElement('input');
+    idInput.type = 'text';
+    //idInput.classList.add('form-control');
+    idInput.classList.add('d-none');
+    idInput.id = 'id';
+    idInput.name = 'id';
+    idInput.value = product.id;
+    cardBodyElement.appendChild(idInput);
+    
+    // Pulsante "Modifica"
+    const modifyButton = document.createElement('input');
+    modifyButton.type = 'submit';
+    modifyButton.classList.add('btn', 'btn-primary');
+    modifyButton.value = 'Modifica';
+    modifyButton.formAction = BASE_URL+'update';
+    cardBodyElement.appendChild(modifyButton);
+
+    // Pulsante "Elimina"
+    const deleteButton = document.createElement('button');
+    deleteButton.type = 'button';
+    deleteButton.classList.add('btn', 'btn-danger');
+    deleteButton.textContent = 'Elimina'; // Utilizza textContent per impostare il testo del pulsante
+    deleteButton.setAttribute('data-toggle', 'modal'); // Aggiungi l'attributo data-toggle
+    deleteButton.setAttribute('data-target', '#deleteModal');
+
+    deleteButton.addEventListener('click', function () {
+        // Mostra la modal di conferma
+        const deleteModal = document.querySelector('#deleteModal');
+        formElement.action = BASE_URL + 'delete';
+
+        // Apri la modal di conferma
+        const modal = new bootstrap.Modal(deleteModal);
+        modal.show();
+
+        // Aggiungi un event listener per l'azione di eliminazione effettiva
+        const confirmDeleteButton = document.querySelector('#confirmDelete');
+        confirmDeleteButton.addEventListener('click', function () {
+            console.log('ciao');
+            // Invia il modulo
+            formElement.submit();
+        });
+    });
+
+    cardBodyElement.appendChild(deleteButton);
+
+    // Aggiunta di tutti gli elementi al form
+    const inputElements = [csrfField, idInput, modifyButton, deleteButton];
+    inputElements.forEach(input => {
+        formElement.appendChild(input);
+    });
+    
     cardElement.appendChild(cardBodyElement);
     cardElement.appendChild(formElement);
     return cardElement;
